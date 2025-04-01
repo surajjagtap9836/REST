f4305bba20fc01b68a67edc87cb05af511a81ca4
surajjagtap9836 <surajjagtap9836@gmail.com>
2025-04-01 18:02:12 +0530

    add options create and view

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
+const { log } = require("console");
 const express =require("express");
 const app=express();
 const path=require("path")
 
 const port=5204;
+app.use(express.urlencoded({extended:true}));//frontend data parse eg:form 
+// app.use(express.json()); // Middleware to parse JSON request body
 
-app.set("view engine","ejs");
+app.set("view engine","ejs");//backend data parse
 app.set("views",path.join(__dirname,"views"));
 
 app.use(express.static(path.join(__dirname,"public")));
 
-app.set(express.urlencoded({extended:true}));
+
 
 let posts=[
-    {
+    {   id:"1a",
         username:"@sonya23sk",
         content:"Hello kartiki...iskon la geli hotis ky...?"
     },
-    {
+    {   id:"2b",
         username:"@kartiki4sk",
         content:"Hello sonya...Hoo iskon la geli hoti mi aaj.."
     },
-    {
+    {   id:"3c",
         username:"@outputresult",
         content:"conversation done successfully..."
     }
@@ -32,4 +35,18 @@ app.listen(port,()=>{
 })
 app.get("/posts",(req,res)=>{
  res.render("index.ejs",{ posts });
+})
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+})
+app.post("/posts",(req,res)=>{
+    let { username , content } = req.body;
+    posts.push({username,content });
+    res.send("Post request working successfully...")
+
+})
+app.get("/posts/:id",(req,res)=>{
+  let {id}=req.params;
+  let post=posts.find((p)=>id===p.id);
+  res.render("show.ejs",{post});
 })
